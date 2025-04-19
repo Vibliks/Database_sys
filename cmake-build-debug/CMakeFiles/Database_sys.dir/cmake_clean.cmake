9c1e18b3c609b62e65803c3eaa84a946afda5571
Vibliks <tymkos.nowicki@gmail.com>
2025-04-19 14:32:58 +0200

    Csv implementacions

---
+++ cmake-build-debug/CMakeFiles/Database_sys.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Database_sys.dir/Csv_main.cpp.obj"
+  "CMakeFiles/Database_sys.dir/Csv_main.cpp.obj.d"
+  "CMakeFiles/Database_sys.dir/main.cpp.obj"
+  "CMakeFiles/Database_sys.dir/main.cpp.obj.d"
+  "Database_sys.exe"
+  "Database_sys.exe.manifest"
+  "Database_sys.pdb"
+  "libDatabase_sys.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Database_sys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
